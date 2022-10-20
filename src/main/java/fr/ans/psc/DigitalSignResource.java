9e32a02072b154a99681cbda86a3782f0b434fcb
pmortas <patrick.mortas.ext@esante.gouv.fr>
2022-10-20 14:51:57 +0200

    cleanup

--- src/main/java/fr/ans/psc/DigitalSignResource.java
+++ src/main/java/fr/ans/psc/DigitalSignResource.java
@@ -4,12 +4,10 @@ import io.gravitee.gateway.api.handler.Handler;
 import io.gravitee.resource.api.AbstractConfigurableResource;
 import io.gravitee.resource.api.ResourceConfiguration;
 
-import java.util.List;
-
 public abstract class DigitalSignResource<C extends ResourceConfiguration> extends AbstractConfigurableResource<C> {
 
-    public abstract void sign(byte[] docToSign, List<AdditionalParameter> additionalParameters, Handler<DigitalSignResponse> responseHandler);
+    public abstract void sign(byte[] docToSign, Handler<DigitalSignResponse> responseHandler);
     
-    public abstract DigitalSignResponse sign(byte[] docToSign, List<AdditionalParameter> additionalParameters);
+    public abstract DigitalSignResponse sign(byte[] docToSign);
 
 }
